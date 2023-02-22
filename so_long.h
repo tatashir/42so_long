1598c7ceedcdd1b7a384b1d84c9487706c3c3564
Taichi Tashiro <tatashir@c1r11s9.42tokyo.jp>
2023-02-22 20:39:54 +0900

    222

--- so_long.h
+++ so_long.h
@@ -6,7 +6,7 @@
 /*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 18:02:32 by tatashir          #+#    #+#             */
-/*   Updated: 2023/02/22 18:05:09 by tatashir         ###   ########.fr       */
+/*   Updated: 2023/02/22 20:39:22 by tatashir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,10 @@
 # include <unistd.h>
 # include <stdlib.h>
 # include <stdio.h>
+
+typedef struct s_solong{
+	void
+	void
+};	t_solong
 
 #endif
