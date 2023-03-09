9799e818d46ecdef9c392362af538500fea0e1ca
taichi tashiro <taichi@taichinoMacBook-Air.local>
2023-03-09 22:32:10 +0900

    move

--- move.c
+++ move.c
@@ -6,7 +6,7 @@
 /*   By: tatashir <tatashir@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/09 17:39:07 by tatashir          #+#    #+#             */
-/*   Updated: 2023/03/09 18:14:38 by tatashir         ###   ########.fr       */
+/*   Updated: 2023/03/09 22:31:53 by tatashir         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,7 +44,7 @@ void	move_up(t_solong *sl, char point)
     init_after_move(sl);
 }
 
-void	move=left(t_solong *sl, char point)
+void	move_left(t_solong *sl, char point)
 {
 	char	c;
 
@@ -54,4 +54,15 @@ void	move=left(t_solong *sl, char point)
 	else if (c == '0')
 	{
 		sl->line[sl->y][sl->x - 1] = 'P';
-		sl->line[sl->
+		sl->line[sl->y][sl->x] = point;
+    }
+    else if (c == 'C')
+    {
+        sl->line[sl->y][sl->x - 1] = 'P';
+        sl->line[sl->y][sl->x] = point;
+        sl->c_cnt--;
+    }
+    else if (c == 'E')
+        change_code(sl, sl->y, sl->x - 1);
+    init_after_move(sl);
+}
